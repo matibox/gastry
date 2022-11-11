2b3222d33bae2aae20159bbf251a30c899916fc3
Matibox <mateusz.hladky@gmail.com>
2022-11-11 20:20:59 +0100

    Add GET /recipes endpoint Change id on Favourite model

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import sensible from '@fastify/sensible';
+import cors from '@fastify/cors';
 import dotenv from 'dotenv';
 import { PrismaClient } from '@prisma/client';
 dotenv.config();
@@ -9,10 +10,24 @@ const PORT = parseInt(process.env.PORT);
 
 const app = Fastify();
 app.register(sensible);
+app.register(cors, {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+});
 
 const prisma = new PrismaClient();
 
-//TODO api routes
+app.get('/recipes', async () => {
+  return await commitToDb(
+    prisma.recipe.findMany({
+      select: {
+        id: true,
+        cookingTime: true,
+        title: true,
+      },
+    })
+  );
+});
 
 async function commitToDb(promise: Promise<unknown>) {
   const [error, data] = await app.to(promise);
