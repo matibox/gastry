da4bb3162e85eb300184c77676c3d477844df558
Matibox <mateusz.hladky@gmail.com>
2022-11-13 17:51:25 +0100

    Add reduced motion preference handling in ChangingText component

--- client/src/features/hero/ChangingText.tsx
+++ client/src/features/hero/ChangingText.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion, useReducedMotion } from 'framer-motion';
 import { useTextChange } from './useTextChange';
 
 interface ChangingTextProps {
@@ -7,10 +7,11 @@ interface ChangingTextProps {
 
 export function ChangingText({ textOptions }: ChangingTextProps) {
   const { displayedText } = useTextChange(textOptions, 5000);
+  const reduceMotion = useReducedMotion();
 
   return (
     <motion.span
-      initial={{ opacity: 0 }}
+      initial={{ opacity: reduceMotion ? 1 : 0 }}
       animate={{ opacity: 1 }}
       transition={{
         ease: 'easeOut',
