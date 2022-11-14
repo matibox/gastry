a188457c1be1d113053e7e49229f50793e394a5a
Matibox <mateusz.hladky@gmail.com>
2022-11-14 19:43:32 +0100

    Extract loadMore logic from useLatestRecieps into useLoadMore

--- client/src/features/LatestRecipes/useLatestRecipes.ts
+++ client/src/features/LatestRecipes/useLatestRecipes.ts
@@ -1,38 +1,20 @@
-import { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAsyncFn } from '../../hooks/useAsync';
+import { useLoadMore } from '../../hooks/useLoadMore';
 import { getLatestRecipes } from '../../services/recipes';
 import Recipe from '../../types/RecipeOverview';
 
 export function useLatestRecipes(quantity: number) {
   const getLatestRecipesFn = useAsyncFn(getLatestRecipes);
+  const { data, ...state } = useLoadMore(quantity, getLatestRecipesFn);
   const [recipes, setRecipes] = useState<Recipe[]>([]);
-  const [offset, setOffset] = useState(0);
-  const [moreToLoad, setMoreToLoad] = useState(true);
 
-  const loadMore = useCallback(() => {
-    if (!moreToLoad) return;
-    getLatestRecipesFn.run(offset, quantity).then(newRecipes => {
-      let length = recipes.length;
-      setRecipes(prevRecipes => {
-        return [
-          ...new Map(
-            [...prevRecipes, ...newRecipes].map(recipe => [recipe.id, recipe])
-          ).values(),
-        ];
-      });
-      if (recipes.length === length) {
-        setMoreToLoad(false);
-        return;
-      }
-      setOffset(prevOffset => prevOffset + quantity);
-    });
-  }, []);
+  useEffect(() => {
+    setRecipes(data);
+  }, [data]);
 
   return {
     recipes,
-    loading: getLatestRecipesFn.loading,
-    error: getLatestRecipesFn.error,
-    loadMore,
-    moreToLoad,
+    ...state,
   };
 }
---
+++ client/src/hooks/useLoadMore.ts
@@ -0,0 +1,41 @@
+import { useCallback, useState } from 'react';
+
+interface ServiceFn {
+  data: undefined;
+  loading: boolean;
+  error: undefined;
+  run: (...params: any[]) => Promise<any>;
+}
+
+export function useLoadMore(quantity: number, serviceFn: ServiceFn) {
+  const [data, setData] = useState<any[]>([]);
+  const [offset, setOffset] = useState(0);
+  const [moreToLoad, setMoreToLoad] = useState(true);
+
+  const loadMore = useCallback(() => {
+    if (!moreToLoad) return;
+    serviceFn.run(offset, quantity).then(newData => {
+      let length = data.length;
+      setData(prevData => {
+        return [
+          ...new Map(
+            [...prevData, ...newData].map(item => [item.id, item])
+          ).values(),
+        ];
+      });
+      if (data.length === length) {
+        setMoreToLoad(false);
+        return;
+      }
+      setOffset(prevOffset => prevOffset + quantity);
+    });
+  }, []);
+
+  return {
+    data,
+    loading: serviceFn.loading,
+    error: serviceFn.error,
+    moreToLoad,
+    loadMore,
+  };
+}
