0f72b359dc502611fc828817627213e42e329d0c
Matibox <mateusz.hladky@gmail.com>
2022-11-14 20:05:04 +0100

    Add useIntersectionObserver hook Implement useIntersectionObserver hook into YourRecipes component

--- client/src/features/YourRecipes/YourRecipes.tsx
+++ client/src/features/YourRecipes/YourRecipes.tsx
@@ -1,17 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { RecipesList } from '../../components/RecipesList/RecipesList';
+import { useIntersectionObserver } from '../../hooks/useIntersectionObserver';
 import { useYourRecipes } from './useYourRecipes';
 import styles from './YourRecipes.module.css';
 
 export function YourRecipes() {
+  const sectionRef = useRef(null);
   const { recipes, loading, error, loadMore, moreToLoad } = useYourRecipes(3);
+  const { isIntersecting } = useIntersectionObserver(sectionRef);
 
   useEffect(() => {
+    if (!isIntersecting) return;
     loadMore();
-  }, [loadMore]);
+  }, [isIntersecting]);
 
   return (
-    <section className={styles.wrapper}>
+    <section className={styles.wrapper} ref={sectionRef}>
       <h2>your recipes</h2>
       <RecipesList
         recipes={recipes}
