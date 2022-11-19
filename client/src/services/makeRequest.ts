abdaf9ce4de44092f8bb2184d3b3e1768070c9b5
Matibox <mateusz.hladky@gmail.com>
2022-11-19 10:37:54 +0100

    Change config type in makeRequest function

--- client/src/services/makeRequest.ts
+++ client/src/services/makeRequest.ts
@@ -1,12 +1,7 @@
+import { AxiosRequestConfig } from 'axios';
 import { api } from '../api/api';
 
-interface MakeRequestConfig {
-  method: string;
-  params?: object;
-  data?: object;
-}
-
-export function makeRequest(url: string, config: MakeRequestConfig) {
+export function makeRequest(url: string, config: AxiosRequestConfig<object>) {
   return api(url, config)
     .then(res => res.data)
     .catch(err => Promise.reject(err.message ?? 'Error'));
