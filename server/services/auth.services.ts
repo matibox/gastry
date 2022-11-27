da65d85f95777274e5bafea754b5fc1c4308a052
Matibox <mateusz.hladky@gmail.com>
2022-11-27 21:19:01 +0100

    Add /login route

--- server/services/auth.services.ts
+++ server/services/auth.services.ts
@@ -1,1 +1,14 @@
 import prisma from '../utils/prisma';
+
+export async function getUserByEmail(email: string) {
+  return await prisma.user.findUnique({
+    where: { email },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      password: true,
+      role: true,
+    },
+  });
+}
