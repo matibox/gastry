4affae7b2a07cd58c2c80e258386685f3084f1f7
Matibox <mateusz.hladky@gmail.com>
2022-11-27 21:40:53 +0100

    Add verify middleware

---
+++ server/middleware/verify.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+export interface CustomRequest extends Request {
+  token: string | JwtPayload;
+}
+
+export function verify(req: Request, res: Response, next: NextFunction) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.status(401).json('Unauthenticated');
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err, user) => {
+    if (err) return res.status(403).json('Token invalid');
+    (req as CustomRequest).token = token;
+  });
+}
