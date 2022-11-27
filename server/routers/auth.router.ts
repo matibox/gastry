0bb7136b664c9da511783c4228f05b7c25b7dbc2
Matibox <mateusz.hladky@gmail.com>
2022-11-27 22:17:57 +0100

    Add zod validation for /register route

--- server/routers/auth.router.ts
+++ server/routers/auth.router.ts
@@ -1,6 +1,17 @@
 import express from 'express';
-import type { Request, Response } from 'express';
+import { z } from 'zod';
 
 export const authRouter = express.Router();
 
-authRouter.post('/login', (req, res) => {});
+const userSchema = z.object({
+  name: z.string().min(6),
+  email: z.string().min(6).email(),
+  password: z.string().min(6),
+});
+
+authRouter.post('/register', async (req, res) => {
+  const validation = userSchema.safeParse(req.body);
+  if (!validation.success) {
+    return res.status(400).json(validation.error.format().name?._errors[0]);
+  }
+});
